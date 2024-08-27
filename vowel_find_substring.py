5ce81ffd65bff290ac2140618e8ed1022caf772b
hasanrahat <rahat.ruet@gmail.com>
2024-08-27 23:44:31 +0600

    Second final Submit

--- vowel_find_substring.py
+++ vowel_find_substring.py
@@ -2,11 +2,11 @@ class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         vowel={'a','e','i','o','u'}
 
-        count,l,res=0,0,0
+        count,l,result=0,0,0
         for r in range(len(s)):
             count+=1 if s[r] in vowel else 0
             if r-l+1>k:
                 count-=1 if s[l] in vowel else 0
                 l+=1
-            res=max(res,count)
-        return res
+            result=max(result,count)
+        return result
